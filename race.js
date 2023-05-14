b68e80b22a8fa2f02c92ce30f7d99bac2da95366
Eunel Soliveres <eunel_soliveres@yahoo.com>
2023-05-14 23:35:33 +0800

    new test files

---
+++ race.js
@@ -0,0 +1,7 @@
+// Synchronous Approach
+console.log(`🐇 finishes!`);
+console.log(`🐢 finishes!`);
+
+// Asynchronous Approach
+setTimeout(() => console.log(`🐇 finishes!`), 0);
+console.log(`🐢 finishes!`);
